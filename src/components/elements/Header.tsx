9ddf12a12d29f61093359ea273181d86d276bd11
nhatnvph <nhatngo20102002@gmail.com>
2023-11-15 20:08:57 +0700

    u

--- src/components/elements/Header.tsx
+++ src/components/elements/Header.tsx
@@ -85,7 +85,7 @@ const Header = () => {
               href="#"
               className="block py-2 px-3 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
             >
-              Concat
+              Concats
             </a>
           </li>
           <li>
