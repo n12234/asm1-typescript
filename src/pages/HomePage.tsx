1b59c8bcf621a13b00c87e819ce5064f1157e987
nhatnvph <nhatngo20102002@gmail.com>
2023-11-17 13:13:36 +0700

    u

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import type { ProductType } from '../types/product';
 import ProductCard from '../components/elements/ProductCard';
 
